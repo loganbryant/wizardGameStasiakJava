347bf9104b582b8b8d71aca6a7d05177aa6dddcf
loganbryant <35818624+loganbryant@users.noreply.github.com>
2024-02-28 18:11:10 -0700

    Update wizardGameSrc.java

--- wizardGameSrc.java
+++ wizardGameSrc.java
@@ -5,24 +5,26 @@ import java.util.Random;
 public class wizardGameSrc {
     private String playerName;
     private int playerTotalHitPoints;
+    private int playerBaseHitPoints;
     private int playerMinHitStrength;
     private int playerMaxHitStrength;
     private boolean playerFireResistance;
     private boolean playerIceResistance;
     private boolean playerTurnSkipped = false;
-    boolean playerTurn = true; 
+    boolean playerTurn = true;
 
-    private String aiName;
-    private int aiTotalHitPoints;
-    private boolean aiFireResistance;
-    private boolean aiIceResistance;
-    private boolean aiTurnSkipped = false;
+    private String[] aiNames = new String[3];
+    private int[] aiTotalHitPoints = new int[3];
+    private boolean[] aiFireResistance = new boolean[3];
+    private boolean[] aiIceResistance = new boolean[3];
+    private boolean[] aiTurnSkipped = new boolean[3];
 
     public void play() {
-        displayAsciiArt();
         getPlayerDetails();
         getAIDetails();
-        fight();
+        for (int i = 0; i < 3; i++) {
+            fightWithAI(i);
+        }
     }
 
     private void getPlayerDetails() {
@@ -36,6 +38,7 @@ public class wizardGameSrc {
 
                 System.out.println("Now please enter your total hit points: ");
                 playerTotalHitPoints = scan.nextInt();
+                playerBaseHitPoints = playerTotalHitPoints;
 
                 System.out.println("Choose a resistance. 1. Fire. 2. Ice");
                 int resistanceChoice = scan.nextInt();
@@ -49,7 +52,7 @@ public class wizardGameSrc {
                 inputGood = false;
             } catch (InputMismatchException ime) {
                 System.out.println("You entered an invalid value. Try again.");
-                scan.next(); 
+                scan.next();
             }
         }
     }
@@ -57,71 +60,73 @@ public class wizardGameSrc {
     private void getAIDetails() {
         Scanner scan = new Scanner(System.in);
 
-        boolean inputGood = true;
-        while (inputGood) {
-            try {
-                System.out.println("Please Enter AI's name: ");
-                aiName = scan.next();
+        for (int i = 0; i < 3; i++) {
+            boolean inputGood = true;
+            while (inputGood) {
+                try {
+                    System.out.println("Please Enter AI " + (i + 1) + "'s name: ");
+                    aiNames[i] = scan.next();
 
-                System.out.println("Now please enter AI's total hit points: ");
-                aiTotalHitPoints = scan.nextInt();
-                System.out.println("Choose AI's resistance. 1. Fire. 2. Ice. 3. Random. ");
-                int resistanceChoice = scan.nextInt();
-                if (resistanceChoice == 1) {
-                    aiFireResistance = true;
-                    aiIceResistance = false;
-                } else if (resistanceChoice == 2) {
-                    aiFireResistance = false;
-                    aiIceResistance = true;
-                } else if (resistanceChoice == 3) {
-                    Random random = new Random();
-                    resistanceChoice = random.nextInt(2) + 1; 
+                    Random rand = new Random();
+                    int minAIHitPoints = (int) (playerTotalHitPoints * 0.8); // 80% of player's hit points
+                    int maxAIHitPoints = (int) (playerTotalHitPoints * 1.2); // 120% of player's hit points
+                    int randomAIHitPoints = rand.nextInt(maxAIHitPoints - minAIHitPoints + 1) + minAIHitPoints;
+                    aiTotalHitPoints[i] = randomAIHitPoints;
+                    int resistanceChoice = 3;
                     if (resistanceChoice == 1) {
-                        aiFireResistance = true;
-                        aiIceResistance = false;
-                    } else {
-                        aiFireResistance = false;
-                        aiIceResistance = true;
+                        aiFireResistance[i] = true;
+                        aiIceResistance[i] = false;
+                    } else if (resistanceChoice == 2) {
+                        aiFireResistance[i] = false;
+                        aiIceResistance[i] = true;
+                    } else if (resistanceChoice == 3) {
+                        Random random = new Random();
+                        resistanceChoice = random.nextInt(2) + 1;
+                        if (resistanceChoice == 1) {
+                            aiFireResistance[i] = true;
+                            aiIceResistance[i] = false;
+                        } else {
+                            aiFireResistance[i] = false;
+                            aiIceResistance[i] = true;
+                        }
                     }
-                } else {
-                    System.out.println("Invalid resistance choice. Please try again.");
-                    continue;
+                    inputGood = false;
+                } catch (InputMismatchException ime) {
+                    System.out.println("You entered an invalid value. Try again.");
+                    scan.next();    
                 }
-                inputGood = false;
-            } catch (InputMismatchException ime) {
-                System.out.println("You entered an invalid value. Try again.");
-                scan.next();
             }
         }
     }
-    private void magicMissilePlayer() {
-        aiTotalHitPoints -= 3;
-        System.out.println("You have cast Magic Missile on the AI, dealing 3 damage.");
+
+    private void magicMissilePlayer(int aiIndex) {
+        aiTotalHitPoints[aiIndex] -= 3;
+        System.out.println("You have cast Magic Missile on " + aiNames[aiIndex] + ", dealing 3 damage.");
     }
 
-    private void fireboltPlayer() {
-        if (!aiFireResistance) {
-            aiTotalHitPoints -= 10;
-            System.out.println("You have cast Firebolt on the AI, dealing 10 damage.");
+    private void fireboltPlayer(int aiIndex) {
+        if (!aiFireResistance[aiIndex]) {
+            aiTotalHitPoints[aiIndex] -= 10;
+            System.out.println("You have cast Firebolt on " + aiNames[aiIndex] + ", dealing 10 damage.");
         } else {
-            aiTotalHitPoints -= 1;
-            System.out.println("You have cast Firebolt on the AI, dealing 1 damage.");
+            aiTotalHitPoints[aiIndex] -= 1;
+            System.out.println("You have cast Firebolt on " + aiNames[aiIndex] + ", dealing 1 damage.");
         }
     }
 
-    private void iceSpikePlayer() {
-        if (!aiIceResistance) {
-            aiTotalHitPoints -= 10;
-            System.out.println("You have cast Ice Spike on the AI, dealing 10 damage.");
+    private void iceSpikePlayer(int aiIndex) {
+        if (!aiIceResistance[aiIndex]) {
+            aiTotalHitPoints[aiIndex] -= 10;
+            System.out.println("You have cast Ice Spike on " + aiNames[aiIndex] + ", dealing 10 damage.");
         } else {
-            aiTotalHitPoints -= 1;
-            System.out.println("You have cast Ice Spike on the AI, dealing 1 damage.");
+            aiTotalHitPoints[aiIndex] -= 1;
+            System.out.println("You have cast Ice Spike on " + aiNames[aiIndex] + ", dealing 1 damage.");
         }
     }
 
-    private void cursePlayer() {
-        aiTurnSkipped = true;
-        System.out.println("You have cursed your opponent. The AI cannot cast this turn.");
+    private void cursePlayer(int aiIndex) {
+        aiTurnSkipped[aiIndex] = true;
+        System.out.println("You have cursed " + aiNames[aiIndex] + ". " + aiNames[aiIndex] + " cannot cast this turn.");
     }
 
     private void healingPlayer() {
@@ -129,44 +134,47 @@ public class wizardGameSrc {
         System.out.println("You have healed yourself, gaining 10 hit points.");
     }
 
-    private void magicMissileAI() {
+    private void magicMissileAI(int aiIndex) {
         playerTotalHitPoints -= 3;
-        System.out.println("AI has cast Magic Missile on you, dealing 3 damage.");
+        System.out.println(aiNames[aiIndex] + " has cast Magic Missile on you, dealing 3 damage.");
     }
 
-    private void fireboltAI() {
+    private void fireboltAI(int aiIndex) {
         if (!playerFireResistance) {
             playerTotalHitPoints -= 10;
-            System.out.println("AI has cast Firebolt on you, dealing 10 damage.");
+            System.out.println(aiNames[aiIndex] + " has cast Firebolt on you, dealing 10 damage.");
         } else {
             playerTotalHitPoints -= 1;
-            System.out.println("AI has cast Firebolt on you, dealing 1 damage.");
+            System.out.println(aiNames[aiIndex] + " has cast Firebolt on you, dealing 1 damage.");
         }
     }
 
-    private void iceSpikeAI() {
+    private void iceSpikeAI(int aiIndex) {
         if (!playerIceResistance) {
             playerTotalHitPoints -= 10;
-            System.out.println("AI has cast Ice Spike on you, dealing 10 damage.");
+            System.out.println(aiNames[aiIndex] + " has cast Ice Spike on you, dealing 10 damage.");
         } else {
             playerTotalHitPoints -= 1;
-            System.out.println("AI has cast Ice Spike on you, dealing 1 damage.");
+            System.out.println(aiNames[aiIndex] + " has cast Ice Spike on you, dealing 1 damage.");
         }
     }
 
-    private void curseAI() {
+    private void curseAI(int aiIndex) {
         playerTurnSkipped = true;
-        System.out.println("AI has cursed you. You cannot cast this turn.");
+        System.out.println(aiNames[aiIndex] + " has cursed you. You cannot cast this turn.");
     }
 
     private void healingAI() {
-        aiTotalHitPoints += 10;
-        System.out.println("AI has healed itself, gaining 10 hit points.");
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(3);
+        aiTotalHitPoints[randomIndex] += 10;
+        System.out.println(aiNames[randomIndex] + " has healed itself, gaining 10 hit points.");
     }
 
-    private void fight() {
+    private void fightWithAI(int aiIndex) {
         Scanner scan = new Scanner(System.in);
-        while (playerTotalHitPoints > 0 && aiTotalHitPoints > 0) {
+        while (playerTotalHitPoints > 0 && aiTotalHitPoints[aiIndex] > 0) {
+
             if (playerTurn) {
                 if (!playerTurnSkipped) {
                     System.out.println("Player, it's your turn to cast a spell. Enter the spell number:");
@@ -174,19 +182,19 @@ public class wizardGameSrc {
                     int spellNumber = scan.nextInt();
                     switch (spellNumber) {
                         case 1:
-                            magicMissilePlayer();
+                            magicMissilePlayer(aiIndex);
                             break;
                         case 2:
-                            fireboltPlayer();
+                            fireboltPlayer(aiIndex);
                             break;
                         case 3:
-                            iceSpikePlayer();
+                            iceSpikePlayer(aiIndex);
                             break;
                         case 4:
                             healingPlayer();
                             break;
                         case 5:
-                            cursePlayer();
+                            cursePlayer(aiIndex);
                             break;
                         default:
                             System.out.println("Invalid spell number. Please try again.");
@@ -197,66 +205,78 @@ public class wizardGameSrc {
                     playerTurnSkipped = false;
                 }
             } else {
-                if (!aiTurnSkipped) {
+                if (!aiTurnSkipped[aiIndex]) {
                     int aiSpellNumber = (int) (Math.random() * 5) + 1;
                     switch (aiSpellNumber) {
                         case 1:
-                            magicMissileAI();
+                            magicMissileAI(aiIndex);
                             break;
                         case 2:
-                            fireboltAI();
+                            fireboltAI(aiIndex);
                             break;
                         case 3:
-                            iceSpikeAI();
+                            iceSpikeAI(aiIndex);
                             break;
                         case 4:
                             healingAI();
                             break;
                         case 5:
-                            curseAI();
+                            curseAI(aiIndex);
                             break;
                     }
                 } else {
-                    System.out.println("AI's turn is skipped due to curse.");
-                    aiTurnSkipped = false;
+                    System.out.println(aiNames[aiIndex] + "'s turn is skipped due to curse.");
+                    aiTurnSkipped[aiIndex] = false;
                 }
             }
 
             playerTurn = !playerTurn;
 
+            if (aiTotalHitPoints[aiIndex] <= 0) {
+                System.out.println("AI " + aiNames[aiIndex] + " has been defeated!");
+                if (aiIndex < 2) {
+                    System.out.println("You have " + playerTotalHitPoints + " hit points remaining. Drink a healing potion to restore yourself to full health? 1. Yes 2. No");
+                    int choice = scan.nextInt();
+                    if (choice == 1) {
+                        playerTotalHitPoints = playerBaseHitPoints;
+                    }
+                }
+            }
             if (playerTotalHitPoints <= 0) {
-                System.out.println("AI Wins");
+                System.out.println("AI " + aiNames[aiIndex] + " Wins");
                 break;
-            } else if (aiTotalHitPoints <= 0) {
-                System.out.println("Player Wins");
+            } else if (aiTotalHitPoints[aiIndex] <= 0) {
+                System.out.println("Player Wins against AI " + aiNames[aiIndex]);
                 break;
             }
         }
     }
 
-public void displayAsciiArt() {
-    System.out.println(
-        "      _,-'|                \n" +
-        "   ,-'._  |                \n" +
-        ".||,      |####\\ |         \n" +
-        "\\.`',/     \\####| |        \n" +
-        "= ,. =      |###| |         \n" +
-        "/ || \\    ,-'\\#/,'`.        \n" +
-        "  ||     ,'   `,,. `.       \n" +
-        "  ,|____,' , ,;' \\| |      \n" +
-        " (3|\\    _/|/'   _| |      \n" +
-        "  ||/,-''  | >-'' _,\\\\     \n" +
-        "  ||'      ==\\ ,-'  ,'     \n" +
-        "  ||       |  V \\ ,|       \n" +
-        "  ||       |    |` |        \n" +
-        "  ||       |    |   \\       \n" +
-        "  ||       |    \\    \\      \n" +
-        "  ||       |     |    \\     \n" +
-        "  ||       |      \\_,-'     \n" +
-        "  ||       |___,,--\")_\\     \n" +
-        "  ||         |_|   ccc/     \n" +
-        "  ||        ccc/             \n" +
-        "  ||                hjm     \n"
-    );
+    public static void displayAsciiArt() {
+        System.out.println(
+                "      _,-'|                \n" +
+                        "   ,-'._  |                \n" +
+                        ".||,      |####\\ |         \n" +
+                        "\\.`',/     \\####| |        \n" +
+                        "= ,. =      |###| |         \n" +
+                        "/ || \\    ,-'\\#/,'`.        \n" +
+                        "  ||     ,'   `,,. `.       \n" +
+                        "  ,|____,' , ,;' \\| |      \n" +
+                        " (3|\\    _/|/'   _| |      \n" +
+                        "  ||/,-''  | >-'' _,\\\\     \n" +
+                        "  ||'      ==\\ ,-'  ,'     \n" +
+                        "  ||       |  V \\ ,|       \n" +
+                        "  ||       |    |` |        \n" +
+                        "  ||       |    |   \\       \n" +
+                        "  ||       |    \\    \\      \n" +
+                        "  ||       |     |    \\     \n" +
+                        "  ||       |      \\_,-'     \n" +
+                        "  ||       |___,,--\")_\\     \n" +
+                        "  ||         |_|   ccc/     \n" +
+                        "  ||        ccc/             \n" +
+                        "  ||                hjm     \n"
+        );
+    }
 }
-}
+
+// a little buggy with the names still but liking the progress overall
